3eba8aac24ffd07f788d3b012070b7424ad25286
Sam Zanca <sam.zanca@generalassemb.ly>
2023-01-09 14:09:22 -0500

    Loop

--- src/index.mjs
+++ src/index.mjs
@@ -8,21 +8,27 @@ import { resolve } from 'path'
 
 const DATA_PATH = resolve('./data.json')
 
-const data = await readFile(DATA_PATH, { encoding: 'utf-8' }).then(str => JSON.parse(str))
+const data = await readFile(DATA_PATH, { encoding: 'utf-8' })
+  .then(str => JSON.parse(str));
 
 // await flashStudentNames(data)
 
 // TODO weighted selection
 
-const selectedIndex = 0
-const selected = data.students[selectedIndex].name
+const MAX_STUDENTS = data.students.length - 1
+
 
 console.log(chalk.bold('Checking for understanding'));
 
-const { understands } = await inquirer.prompt([{
-  name: 'understands',
-  message: `Does ${chalk.blue(selected)} undestand?`,
-  type: 'confirm'
-}])
-
-understands
+while(true) {
+  const selectedIndex = Math.floor(Math.random() * MAX_STUDENTS) + 1;
+  const selected = data.students[selectedIndex].name
+  
+  const { understands } = await inquirer.prompt([{
+    name: 'understands',
+    message: `Does ${chalk.blue(selected)} undestand?`,
+    type: 'confirm'
+  }])
+  
+  understands
+}
